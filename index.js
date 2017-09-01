255aac92a57cec4881ce1f0de3b4ab85c856aa91
sjbavier <sjbavier@gmail.com>
2017-08-31 23:49:37 -0400

    messed with too many things

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
 var convertToJSON = require('./lib/convertToJSON.js');
-var file = process.argv[2];
+var path = require('path');
+// var file = process.argv[2];
 exports.bookmarkTools = convertToJSON(file);
+convertToJson('bookmarks_8_31_17.html',
+{
+  pipeFile: 'bookmarks2.json'
+});
