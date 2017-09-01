b80c61c9e667ba0815c90ea191cce094939d9a33
sjbavier <sjbavier@gmail.com>
2017-09-01 19:15:34 -0400

    getting module ready for npm

--- index.js
+++
@@ -1,8 +0,0 @@
-
-var convertToJSON = require('./lib/convertToJSON.js');
-// var file = process.argv[2];
-exports.bookmarkTools = convertToJSON.init;
-convertToJson.init('bookmarks_8_31_17.html',
-{
-  pipeFile: 'bookmarks2.json'
-});
