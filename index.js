9efb9f48151816c4ee128d1ae1f374d18e3eaed1
sjbavier <sjbavier@gmail.com>
2017-09-01 00:03:01 -0400

    more

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 var convertToJSON = require('./lib/convertToJSON.js');
-var path = require('path');
 // var file = process.argv[2];
-exports.bookmarkTools = convertToJSON(file);
-convertToJson('bookmarks_8_31_17.html',
+exports.bookmarkTools = convertToJSON.init;
+convertToJson.init('bookmarks_8_31_17.html',
 {
   pipeFile: 'bookmarks2.json'
 });
